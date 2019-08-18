a8159b7cfeac5a2d9e35ed9c3f31785823ac4870
tom.debek@gmail.com <tom.debek@gmail.com>
2019-08-18 22:34:47 +0200

    New controller and views workout

---
+++ app/controllers/workouts_controller.rb
@@ -0,0 +1,60 @@
+class WorkoutsController < ApplicationController
+
+  before_action :find_workout, only: [:show, :edit, :update, :destroy, :upvote]
+
+
+  def index
+    @workouts = Workout.all
+  end
+
+  def new
+    @workout = Workout.new
+  end
+
+  def show
+  end
+
+  def edit
+  end
+
+  def create
+    @workout = Workout.new(workout_params)
+    if @workout.save
+      redirect_to @workout
+      flash[:notice] = "Workout successfully created"
+    else
+      render action: 'new'
+    end
+  end
+
+  def update
+    @workout.update(workout_params)
+    if @workout.save
+      redirect_to @workout
+      flash[:notice] = "Workout successfully Updated"
+    else
+      render action: 'edit'
+    end
+  end
+
+  def destroy
+    @workout.destroy
+    redirect_to root_path
+    flash[:notice] = "Workout deleted"
+  end
+
+
+  private
+
+  def workout_params
+    params.require(:workout).permit(:date, :workout, :mood, :length)
+
+  end
+
+  def find_workout
+    @workout = Workout.find(params[:id])
+  end
+
+end
+
+
